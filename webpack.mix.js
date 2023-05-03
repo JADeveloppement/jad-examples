a5a304d66de08498485956b3c1699a36629e527a
jadeveloppement <jadeveloppement@outlook.fr>
2023-05-03 02:06:29 +0200

    added browsersync npm minux style tag in head welcome page

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,11 @@
 let mix = require('laravel-mix');
 
 require ('mix-tailwindcss');
+require('browser-sync');
 
 mix.js('resources/js/app.js', 'public/js')
-.sass('resources/scss/app.scss', 'public/css').tailwind('./tailwind.config.js');
+.sass('resources/scss/app.scss', 'public/css').tailwind('./tailwind.config.js')
+.browserSync({
+    proxy: 'localhost:8000',
+    notify: false
+ });;
