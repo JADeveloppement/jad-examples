4d97ad1eefef274850b2c0d37fb1a26fcddfdae5
jadeveloppement <jadeveloppement@outlook.fr>
2023-05-11 21:59:41 +0200

    creation ada

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,8 @@ mix.js('resources/js/jadeveloppement/app.js', 'public/js/jadeveloppement')
 .sass('resources/scss/democrite/app_index.scss', 'public/css/democrite')
 .js('resources/js/democrite/app_profil.js', 'public/js/democrite')
 .sass('resources/scss/democrite/app_profil.scss', 'public/css/democrite')
+.js('resources/js/ada/app_index.js', 'public/js/ada')
+.sass('resources/scss/ada/app_index.scss', 'public/css/ada')
 .tailwind('./tailwind.config.js')
 .browserSync({
     proxy: 'localhost:8000',
